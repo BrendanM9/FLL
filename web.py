abfcdcbb1c618bf3267460cacd54cadc46d493a1
BrendanM9 <124943530+BrendanM9@users.noreply.github.com>
2023-10-31 15:40:14 -0700

    Add files via upload

---
+++ web.py
@@ -0,0 +1,19 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import time
+#def do_GET():
+'''
+def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+'''
+import http.server
+import socketserver
+
+PORT = 80
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
